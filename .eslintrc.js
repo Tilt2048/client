463eb3a442f54da3a37bbdc03c811fa353fd2413
imheeseung <gmltmd519@gmail.com>
2024-03-11 13:28:00 +0900

    Feat: Create boiler plate

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:prettier/recommended",
+    "react-app",
+  ],
+  plugins: ["react", "prettier"],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2020,
+    sourceType: "module",
+  },
+  rules: {
+    "prettier/prettier": "error",
+    "react/jsx-uses-react": "off",
+    "react/react-in-jsx-scope": "off",
+  },
+  env: {
+    es6: true,
+    node: true,
+    browser: true,
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
